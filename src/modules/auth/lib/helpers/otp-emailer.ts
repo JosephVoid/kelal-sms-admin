0b584dc1e7b3d9da9ee3f9afd0e2f85ed9f64efb
Yoseph <yosephten@gmail.com>
2025-07-13 23:48:31 +0300

    `fix`: change sending domain

--- src/modules/auth/lib/helpers/otp-emailer.ts
+++ src/modules/auth/lib/helpers/otp-emailer.ts
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export async function sendOtpEmail(email: string, otp: string) {
   return await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "noreply@buyersfirst.et",
     to: email,
     subject: "Your OTP Code",
     html: `<p>Your OTP code is <strong>${otp}</strong>. It will expire in 5 minutes.</p>`,
